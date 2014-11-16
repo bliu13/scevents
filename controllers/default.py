52133d0af79c30986ac413bf2a37760395a257b3
Benson Liu <vandeft@gmail.com>
2014-11-15 19:40:39 -0800

    Made minor changes. Displayed name of post rather than the post's id in View. Changed some comments in controller.

--- controllers/default.py
+++ controllers/default.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
 
 #########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - api is an example of Hypermedia API support and access control
+## This is the controller for scevents
 #########################################################################
 
 def index():
@@ -201,7 +196,7 @@ def call():
     return service()
 
 
-@auth.requires_login() 
+@auth.requires_login()
 def api():
     """
     this is example of API with access control
